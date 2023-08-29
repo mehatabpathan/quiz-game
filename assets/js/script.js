a01ecf87c973b66a465aeeeb1ba23e959b025c77
mehatabpathan <>
2023-08-29 22:01:37 +0000

    adding exit and continue button onclick functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,7 @@ const timeCount = document.querySelector(".timer .timer_sec");
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show the info box
 };
+
 // if ExitQuiz button clicked
 exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide the info box
