bde9363d28f7dd3db33961059f23034e37b564d6
mehatabpathan <>
2023-08-29 21:11:33 +0000

    adding questions .js file

---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Where is Estonia located?",
+        answer: "Northern Europe",
+        options: [
+            "Asia",
+            "Northern Europe",
+            "The Balkan peninsula",
+            "Africa"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the Estonian sport called 'kiiking' involved?",
+        answer: "Participants attach themselves to giant steel swings that rotate a full 360 degrees",
+        options: [
+            "Triathlone with sauna breaks",
+            "Knitting and crocheting while jogging",
+            "Car racing on an iced oversea",
+            "Participants attach themselves to giant steel swings that rotate a full 360 degrees"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many islands and islets does Estonia have in the Baltic sea?",
+        answer: "Over 2000",
+        options:[
+            "Over 2000",
+            "None",
+            "Five",
+            "one hundred"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What was invented in Estonia?",
+        answer: "Skype, the online video call service",
+        options: [
+            "Musical instrument Saxophone",
+            "The first antibiotic substance, Penicillin",
+            "The Steam Engine",
+            "Skype, the online video call service"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What can you do with an e-Residency in Estonia?",
+        answer: "It allows entrepreneurs from all over the world to start an EU-based company and manage their business entirely online",
+        options: [
+            "It allows entrepreneurs from all over the world to start an EU-based company and manage their business entirely online",
+            "It allows free travel tickets for non-Estonian residence",
+            "It allows you to get all items starting with letter E for free",
+            "It allows you to use saunas entirely online"
+        ]
+    },
+];
