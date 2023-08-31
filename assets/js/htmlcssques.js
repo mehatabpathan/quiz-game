016b7ca531aa79c9ff0a423342146aec966cac1e
mehatabpathan <>
2023-08-31 10:08:47 +0000

    html and css questions

---
+++ assets/js/htmlcssques.js
@@ -0,0 +1,116 @@
+//creating an array and passing the number, questions, options, and answers
+let question1 = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+                
+              "Hyper Text Markup Language", 
+               "Home Tool Markup Language", 
+               "Hyperlinks and Text Markup Language",
+               "Home Text Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who is making the Web standards?",
+        answer: "The World Wide Web Consortium",
+        options: [
+            
+             "Mozilla",
+             "Microsoft",
+             "The World Wide Web Consortium",
+             "Internet Explorer"
+        ]
+     },
+    {
+        numb: 3,
+        question: "Choose the correct HTML tag for the largest heading",
+        answer: "H1",
+        options:[
+            "Head",
+             "H1",
+             "H6",
+             "Heading"
+            ]
+    },
+    {
+        numb: 4,
+        question: "What is the correct HTML tag for inserting a line break?",
+        answer: " br /",
+        options: [
+              "break /",
+              "br /",
+              "lb /",
+              "br /br"
+             ]
+    },
+    {
+        numb: 5,
+        question: "Choose the correct HTML tag to make a text bold.",
+        answer: "b",
+        options: [
+            "bold",
+             "b",
+             "bb",
+             "break"
+            ]
+    },
+    {
+        numb: 6,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: [
+                 "Creative Style Sheets",
+                "Colorful Style Sheets",
+                 "Computer Style Sheets",
+                 "Cascading Style Sheets"
+
+            ]
+    },
+    {
+        numb: 7,
+        question: "Which is the correct CSS syntax? ",
+        answer: "Body {color: black}",
+        options: [
+                "Body:color=black",
+                "{body;color:black}",
+             "{body:color=black(body}",
+             "Body {color: black}"
+             ]
+    },
+    {
+        numb: 8,
+        question: "How do you insert a comment in a CSS file?",
+        answer: "/* this is a comment */",
+        options: [
+            "/* this is a comment */",
+            " // this is a comment",
+            "  // this is a comment",
+            "// this is a comment //"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which CSS property controls the text size?",
+        answer: "Font-size",
+        options: [
+                "Font-style",
+                "Text-style",
+                "text-size text-size",
+                "Font-size"
+            ]
+    },
+    {
+        numb: 10,
+        question: "How do you change the text color of an element? ",
+        answer: "Color:",
+        options: [
+                 "Color:",
+                "Text-color=",
+                 "Text-color:",
+                 "Fgcolor:"
+            ]
+    },
+];
