e637cd2cd585ce59db0cbdff1de02534ebf8b5b4
mehatabpathan <>
2023-10-14 14:17:51 +0000

    options in to the span

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,10 +224,19 @@ function nextClick() {
 function showQuestions(index){
     const que_text = document.querySelector(".que_text");
     let que_tag = '<span>'+ questions[index].numb+ "." + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-                      +  '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-                      + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-                      + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    let option_tag =
+  '<div class="option"><span>' +
+  questions[index].options[0] +
+  '</span></div>' +
+  '<div class="option"><span>' +
+  questions[index].options[1] +
+  '</span></div>' +
+  '<div class="option"><span>' +
+  questions[index].options[2] +
+  '</span></div>' +
+  '<div class="option"><span>' +
+  questions[index].options[3] +
+  '</span></div>';
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
 
